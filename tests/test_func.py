c148fa3a4e540d0ebe8648f5f4e52d06c7dca482
TevinThuku <tevinthuku@gmail.com>
2019-02-06 17:18:41 +0300

    [finishes #id] test minus fn

--- tests/test_func.py
+++ tests/test_func.py
@@ -2,5 +2,13 @@ def fun(a, b):
     return a + b
 
 
-def test_answer():
+def minus(a, b):
+    return b - a
+
+
+def test_addition():
     assert fun(3, 5) == 8
+
+
+def test_minus():
+    assert minus(2, 5) == 3
